95396257fa211e90997c501f872c680263c15b8a
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-08-10 21:56:06 +0900

    fileを開く

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
+  let files
 </script>
 
 <main>
   <h1>Pixel Art Converter</h1>
+  <input type="file" bind:files />
+
+  {#if files && files[0]}
+    <p>
+      {files[0].name}
+    </p>
+  {/if}
 </main>
